8c62c3a316821121a0b289d5019df24104e57871
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2018-02-14 04:06:11 +0200

    DOcker-Composer working

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors = require('kcors');
 
 const appId = process.env.APPID || '4825a558f5791dd9a89cbfbd03ee206f';
 const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
-const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,FI';
 
 const port = process.env.PORT || 9000;
 
@@ -14,16 +14,33 @@ const app = new Koa();
 app.use(cors());
 
 const fetchWeather = async () => {
-  const endpoint = mapURI + '/weather?q=' + targetCity + '&APPID=' + appId + '&';
+  var endpoint = mapURI + '/weather?q=' + targetCity + '&APPID=' + appId + '&';
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
+const forecast = async () => {
+  var endpoint = mapURI + '/forecast/daily?id=658225&APPID=' + appId + '&cnt=3';
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
-  const weatherData = await fetchWeather();
+  var ip = require('ip');
+  console.log('IP Address', ip.address());
+  const weatherData = await fetchWeather('');
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get('/api/forecast', async ctx => {
+  var ip = require('ip');
+  console.log('IP Address', ip.address());
+  const weatherData = await fetchWeather('forcast');
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData;
 });
 
 app.use(router.routes());
@@ -31,4 +48,4 @@ app.use(router.allowedMethods());
 
 app.listen(port);
 
-console.log('App listening on port' + port);
+console.log('App listening on port ' + port);
