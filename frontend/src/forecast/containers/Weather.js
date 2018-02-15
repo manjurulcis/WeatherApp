3d05c7add45df03915a578d7a9de2695ba4d3af3
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2018-02-15 21:57:24 +0200

    Creating forecast UI and chart

--- frontend/src/forecast/containers/Weather.js
+++ frontend/src/forecast/containers/Weather.js
@@ -12,6 +12,18 @@ export default class SelectCity extends React.Component {
             reportData: < PreLoader / >
         }
     }
+
+    componentWillMount() {
+    }
+
+    componentDidMount() {
+        // Get City ID
+        window.selectedCity = window.selectedCity !== undefined ? window.selectedCity : 'Helsinki';
+        const cityID = Cities.getCityID(window.selectedCity);
+        // Dispatch GET request to server and provide CallBack function
+        $.ajax(WeatherApi(cityID, this.handleServerData.bind(this)));
+    }
+
     render() {
         return (
             <div> {this.state.reportData} </div>
