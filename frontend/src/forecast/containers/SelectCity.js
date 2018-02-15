fe9efb7dd4149c4216a039b43f9f16888da59e73
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2018-02-15 22:01:56 +0200

    Creating necessary components and service

--- frontend/src/forecast/containers/SelectCity.js
+++ frontend/src/forecast/containers/SelectCity.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';;
+import { Link } from 'react-router-dom';
+import SelectOptions from '../components/SelectOptions';
+import SuccessButton from '../components/SuccessButton';
+import Cities from '../services/Cities';
 
 /*
     Page Container fto Select City
@@ -7,9 +10,16 @@ import { Link } from 'react-router-dom';;
 
 let cities;
 export default class SelectCity extends React.Component{
+    handleSelect(){
+        window.selectedCity = cities[$('select').val()];
+        //console.log(window.selectedCity);
+    }
     render (){
+        cities = Cities.getCities();
         return (
                 <div className='row sc-container'>
+                    <div className='col s6 offset-s1'><SelectOptions options={cities} handleSelect={this.handleSelect}/></div>
+                    <Link to='/weather'><div className='col s4 enter-button'><SuccessButton name='Enter' /></div></Link>
                 </div>
         );
     }
