42f20e3734d7f3b1714bc6e5e849ab72be67801f
Md Manzurul Haque <haque.mdmanzurul@gmail.com>
2018-02-19 22:40:42 +0200

    detect user location

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -67,6 +67,59 @@ class Weather extends React.Component {
     this.componentWillMount();
   }
 
+  componentDidMount(){
+    if (navigator.geolocation) {
+      // geolocation is available
+      console.log('working geo location')
+      navigator.geolocation.getCurrentPosition(
+
+        // Success callback
+        function(position) {
+            console.log(position)
+            /*
+            position is an object containing various information about
+            the acquired device location:
+    
+            position = {
+                coords: {
+                    latitude - Geographical latitude in decimal degrees.
+                    longitude - Geographical longitude in decimal degrees. 
+                    altitude - Height in meters relative to sea level.
+                    accuracy - Possible error margin for the coordinates in meters. 
+                    altitudeAccuracy - Possible error margin for the altitude in meters. 
+                    heading - The direction of the device in degrees relative to north. 
+                    speed - The velocity of the device in meters per second.
+                }
+                timestamp - The time at which the location was retrieved.
+            }
+            */
+    
+        },
+    
+        // Optional error callback
+        function(error){
+    
+            /* 
+            In the error object is stored the reason for the failed attempt:
+    
+            error = {
+                code - Error code representing the type of error 
+                        1 - PERMISSION_DENIED
+                        2 - POSITION_UNAVAILABLE
+                        3 - TIMEOUT
+    
+                message - Details about the error in human-readable format.
+            }
+            */
+    
+        }
+    );
+    } 
+    else {
+      // geolocation is not supported
+    }
+  }
+
   render() {
     const { icon } = this.state;
 
